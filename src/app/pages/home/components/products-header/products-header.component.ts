b7df00f72e82d130b7b0a26bb8aff9236b3e9480
Rodion Sibov <rodionsibov@gmail.com>
2023-05-29 21:13:19 +0300

    Building home page

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
-  styleUrls: ['./products-header.component.css']
+  styleUrls: ['./products-header.component.css'],
 })
-export class ProductsHeaderComponent {
+export class ProductsHeaderComponent implements OnInit {
+  sort = 'desc';
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
 }
